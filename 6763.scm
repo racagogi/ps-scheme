73e31a4db5116bd3865cd2bf0b55a39a5d52fe22
racagogi <racagogi@gmail.com>
2023-04-08 08:19:19 +0900

    solve 6763

---
+++ 6763.scm
@@ -0,0 +1,6 @@
+(let ((a (read))
+      (b (read)))
+ (cond ((<= b a) (print "Congratulations, you are within the speed limit!"))
+       ((<= b (+ a 20)) (print "You are speeding and your fine is $100."))
+       ((<= b (+ a 30)) (print "You are speeding and your fine is $270."))
+       (else (print "You are speeding and your fine is $500."))))
